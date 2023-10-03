659ba52f3b0a8eaf7922b901be1210ec38e3dccc
AlexeyKonstantinov <kotrisius@gmail.com>
2023-10-03 21:56:15 +0600

    add endings menu

--- game.js
+++ game.js
@@ -1,18 +1,26 @@
 const mainMenu = document.getElementById("main-menu");
+const endingsMenu = document.getElementById("endings-menu");
 const startBtn = document.getElementById("start-btn");
+const endingsBtn = document.getElementById("endings-btn");
+const backToMenuBtn = document.getElementById("back-to-menu-btn");
 const finishBtn = document.getElementById("finish-button")
 const passageElement = document.getElementById("passage");
 const passageImage = document.getElementById("passage-image");
 const passageText = document.getElementById("passage-text");
 const optionButtons = document.getElementById("option-buttons");
+const endingsContainer = document.getElementById("endings-container");
 
 let passages;
+let openedEndings = [];
+openedEndings.push("end_1") //тестовая концовка
 
 fetch('story_1.json')
     .then(response => response.json())
     .then(jsonResponse => passages = jsonResponse.passages);
 
 startBtn.addEventListener("click", startGame);
+endingsBtn.addEventListener("click", openEndingsMenu)
+backToMenuBtn.addEventListener("click", backToMenu)
 
 function startGame() {
     console.log("start-game");
@@ -28,6 +36,20 @@ function loadPassage(passageName)
 {
     var passage = passages.find(x => x.name == passageName);
     passageText.innerHTML = passage.cleanText
+    
+    let tagsArray = passage.tags.split(" ");
+    let imgTag = tagsArray.find(tag => tag.startsWith("img"));
+    let endTag = tagsArray.find(tag => tag.startsWith("tag"));
+    if (imgTag) {
+      var imgNumber = imgTag.slice(3);
+      passageImage.src = "resources/" + imgNumber + ".jpg";
+    } else {
+      passageImage.src = "resources/cat.jpeg";
+    }
+
+    if(endTag){
+      openedEnding.push(endTag)
+    }
 
     while(optionButtons.firstChild) {
         optionButtons.removeChild(optionButtons.firstChild);
@@ -40,4 +62,26 @@ function loadPassage(passageName)
         button.addEventListener('click', () => loadPassage(link.passageName));
         optionButtons.appendChild(button);
     });
+}
+
+function openEndingsMenu()
+{
+  mainMenu.style.display = "none";
+  endingsMenu.style.display = "block";
+
+  for (const endingElement of endingsContainer.children) {
+    console.log(endingElement.id)
+    if (openedEndings.includes(endingElement.id)){
+      endingElement.className = "ending-button-opened"
+    }
+    else {
+      endingElement.className = "ending-button-closed"
+    }
+  }
+}
+
+function backToMenu()
+{
+  mainMenu.style.display = "flex";
+  endingsMenu.style.display = "none";
 }
